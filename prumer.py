e4130af3f31eccea064fdc82d2ef82d1044143f2
Ondrej-Lesak <147913028+Ondrej-Lesak@users.noreply.github.com>
2023-10-14 12:18:08 +0200

    Update prumer.py

--- prumer.py
+++ prumer.py
@@ -1,1 +1,9 @@
+import sys
 
+def prumer() -> float:
+    cislo = 0
+    for i in range(1,len(sys.argv) - 1):
+            cislo = sys.argv[i] + cislo
+    return(cislo/len(sys.argv))
+
+print(prumer)

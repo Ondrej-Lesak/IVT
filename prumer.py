f34e35f7f18eb6d6e54b943f5e234beb1ec47ba3
Ondrej-Lesak <lesako@arcig.cz>
2023-10-30 14:46:28 +0100

    uprava2

--- prumer.py
+++ prumer.py
@@ -1,9 +1,9 @@
 import sys
 
 def prumer() -> float:
-    nummero = 0
+    cislo = 0
     for i in range(1,len(sys.argv) - 1):
-            nummero = sys.argv[i] + nummero
-    return(nummero/len(sys.argv))
+            cislo = sys.argv[i] + cislo
+    return(cislo/len(sys.argv))
 
 print(prumer)
